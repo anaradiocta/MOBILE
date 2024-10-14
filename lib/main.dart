5c2569fa893217db7b7ff06869ff8899965fa189
anaradiocta <nelaocta12@gmail.com>
2024-10-14 20:29:49 +0700

    Jobsheet 6 - Praktikum 2

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,16 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    // Membuat button section
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: ANARADI OCTA LAVECHIA - 2241760007',
       home: Scaffold(
@@ -58,10 +68,33 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
+            titleSection,
+            buttonSection, // Tambahkan buttonSection ke body
           ],
         ),
       ),
-      
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: Colors.blue),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+
     );
   }
 }
